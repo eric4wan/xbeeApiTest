20ae954044d1537b2b6576d81a94c521577baa3b
Eric Wan <eric4wan@gmail.com>
2015-09-06 13:41:30 -0400

    accepts full payload, working on unforcing AT mode

--- src/com/digi/xbee/example/MainApp.java
+++ src/com/digi/xbee/example/MainApp.java
@@ -11,27 +11,28 @@ public class MainApp {
     private static final String DATA_TO_SEND = "Hello XBee World!";
      
     public static void main(String[] args) throws XBeeException {
-        XBeeDevice myDevice = new XBeeDevice(PORT, BAUD_RATE);
-        byte[] dataToSend = DATA_TO_SEND.getBytes();
-         
-        try {
-            myDevice.open();
-            System.out.format("Sending broadcast data: '%s'", new String(dataToSend));
-             
-            myDevice.sendBroadcastData(dataToSend);
-            System.out.println(" >> Success");
-             
-        } catch (XBeeException e) {
-            System.out.println(" >> Error");
-            e.printStackTrace();
-            System.exit(1);
-        } finally {
-            myDevice.close();
-        }
+//        XBeeDevice myDevice = new XBeeDevice(PORT, BAUD_RATE);
+//        byte[] dataToSend = DATA_TO_SEND.getBytes();
+//         
+//        try {
+//            myDevice.open();
+//            System.out.format("Sending broadcast data: '%s'", new String(dataToSend));
+//             
+//            myDevice.sendBroadcastData(dataToSend);
+//            System.out.println(" >> Success");
+//             
+//        } catch (XBeeException e) {
+//            System.out.println(" >> Error");
+//            e.printStackTrace();
+//            System.exit(1);
+//        } finally {
+//            myDevice.close();
+//        }
     	
     	// Instantiate an XBee device object.
-    	XBeeDevice myXBeeDevice = new XBeeDevice("COM3", 9600);
+    	XBeeDevice myXBeeDevice = new XBeeDevice("/dev/tty.usbserial-DA01M6QM", 9600);
     	myXBeeDevice.open();
+       	System.out.println(myXBeeDevice.getOperatingMode());
     	 
     	// Create the data reception listener.
     	MyDataReceiveListener myDataReceiveListener = new MyDataReceiveListener();
